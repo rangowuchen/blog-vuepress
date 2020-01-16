6e3eec7eddbec2c62aad58fd65400dc0d838b254
LAPTOP-AADIG9CF\简&单 <wuchen@ikuijia.com>
2020-01-16 17:56:39 +0800

    配置文件修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@
  * @Author: wuchen
  * @Date: 2020-01-16 15:55:00
  * @LastEditors  : wuchen
- * @LastEditTime : 2020-01-16 16:39:36
+ * @LastEditTime : 2020-01-16 17:25:48
  * @Description: 
  * @Email: rangowu@163.com
  */
@@ -15,7 +15,8 @@ module.exports = {
             href: `/favicon.ico`
         }]
     ],
-    dest: './docs/.vuepress/dist',
+    // dest: './docs/.vuepress/dist',
+    base: "/blog-vuepress/",
     ga: '',
     evergreen: true,
 }
