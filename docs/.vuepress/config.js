dc9480b9d2a50c14ddba00dbc21d676d76a497bd
LAPTOP-AADIG9CF\简&单 <wuchen@ikuijia.com>
2020-01-16 16:48:45 +0800

    项目初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+/*
+ * @Author: wuchen
+ * @Date: 2020-01-16 15:55:00
+ * @LastEditors  : wuchen
+ * @LastEditTime : 2020-01-16 16:39:36
+ * @Description: 
+ * @Email: rangowu@163.com
+ */
+module.exports = {
+    title: '简&单',
+    description: 'Hello, my friend!',
+    head: [
+        ['link', {
+            rel: 'icon',
+            href: `/favicon.ico`
+        }]
+    ],
+    dest: './docs/.vuepress/dist',
+    ga: '',
+    evergreen: true,
+}
